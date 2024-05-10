3129dacddddd797d866a2d413b38c52f91b0909a
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-05-10 18:38:24 -0300

    Add Address model

---
+++ NewTicketSeller.Domain/Models/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTicketSeller.Domain.Models;
+
+public class Address
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(150, ErrorMessage ="Max characters permited: 150")]
+    public string Street { get; set; } = string.Empty;
+    [Required]
+    [StringLength(10, ErrorMessage = "Max characters permited: 10")]
+    public string Number { get; set; } = string.Empty;
+    [StringLength(100, ErrorMessage = "Max characters permited: 100")]
+    public string? Complement { get; set; }
+    [StringLength(100, ErrorMessage = "Max characters permited: 100")]
+    public string? Reference { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Max characters permited: 50")]
+    public string City { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, ErrorMessage = "Max characters permited: 50")]
+    public string State { get; set; } = string.Empty;
+    [Required]
+    [StringLength(8, ErrorMessage = "Max characters permited: 8")]
+    [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Invalid postcode.")]
+    public string PostalCode { get; set; } = string.Empty;
+}
