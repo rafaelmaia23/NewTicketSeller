badd5574ff11f5e946420a37b913a7774e0f81b3
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-05-13 13:41:49 -0300

    add Find method to IRepository

--- NewTicketSeller.Domain/IRepository/IRepository.cs
+++ NewTicketSeller.Domain/IRepository/IRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace NewTicketSeller.Domain.IRepository;
+﻿using System.Linq.Expressions;
+
+namespace NewTicketSeller.Domain.IRepository;
 
 public interface IRepository<T> where T : class
 {
     Task<IEnumerable<T>?> GetAllAsync();
     Task<T?> GetAsync(int id);
+    Task<IEnumerable<T>?> FindAsync(Expression<Func<T, bool>> expression);
     Task CreateAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
