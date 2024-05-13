c686353790cd42836ba8aa7b0fc99e3beaf7fd0d
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-05-13 12:27:33 -0300

    Add Cinema Model

---
+++ NewTicketSeller.Domain/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewTicketSeller.Domain.Models;
+
+public class Cinema
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100, ErrorMessage = "Max characters permited: 100")]
+    public string Name { get; set; }
+    [Required]
+    [ForeignKey("Address")]
+    public int AddressId { get; set; }
+    public Address Address { get; set; }
+}
