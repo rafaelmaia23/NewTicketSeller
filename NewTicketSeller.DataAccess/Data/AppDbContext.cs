afd14d2f8908077f67807a4f799bd7edcb992868
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-05-13 13:16:51 -0300

    Removed unecessary usings

--- NewTicketSeller.DataAccess/Data/AppDbContext.cs
+++ NewTicketSeller.DataAccess/Data/AppDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NewTicketSeller.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NewTicketSeller.DataAccess.Data;
 
